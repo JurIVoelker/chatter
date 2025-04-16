8d7b35d4f4085f51f5c51d3d2c5c92d7cd3ac2c3
JurIVoelker <juri.voelker@gmx.de>
2025-04-16 14:34:03 +0200

    feat: add logging control to asyncLog function

--- src/utils/logUtils.ts
+++ src/utils/logUtils.ts
@@ -1,20 +1,21 @@
 export const asyncLog = async (message: string) => {
-  const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } = process.env;
+  const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, LOGGING_ENABLED } = process.env;
   const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
   const data = {
     chat_id: TELEGRAM_CHAT_ID, // The chat ID where the message will be sent
     text: message, // The message content
   };
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error("Error sending message:", error);
-    });
+  if (LOGGING_ENABLED === "true")
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error sending message:", error);
+      });
 };
